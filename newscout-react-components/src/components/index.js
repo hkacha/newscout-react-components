5f27414ad05777255b164b2cb6498c85c463ee49
hardik <hardik@fafadiatech.com>
2019-11-06 17:54:49 +0530

    added props value

--- newscout-react-components/src/components/index.js
+++ newscout-react-components/src/components/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class HelloWorld extends Component {
 	render(){
 		return(
-			<h1>HelloWorld new</h1>
+			<h1>{this.props.title}</h1>
 		)
 	}
 }
