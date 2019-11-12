f3e59659feb61c7b3e23d8aafe30889f7bf3c614
hardik <hardik@fafadiatech.com>
2019-11-12 10:57:46 +0530

    added new app

---
+++ newscout/src/CardItem/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+
+export default class CardItem extends React.Components {
+	// const {image_path, card_title, card_text} = this.props;
+	render(){
+		return(
+			<Card>
+				<CardImg top width="100%" src="/assets/318x180.svg" alt="card_title" />
+				<CardBody>
+					<CardText>
+						<small className="text-muted">Last updated 3 mins ago</small>
+					</CardText>
+					<CardTitle>card_title</CardTitle>
+					<CardText>card_text</CardText>
+				</CardBody>
+			</Card>
+		)
+	}
+}
