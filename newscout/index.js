04b60ea213c38e655324bef98d1b7934c1cad17d
hardik <hardik@fafadiatech.com>
2019-11-12 12:39:42 +0530

    added index.css and font

--- newscout/index.js
+++ newscout/index.js
@@ -7,6 +7,8 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+require("./index.css");
+
 var _reactstrap = require("reactstrap");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
