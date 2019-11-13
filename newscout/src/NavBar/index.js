da47a99dc6cf20fc653c5964ecbfdb83435dd98b
hardik <hardik@fafadiatech.com>
2019-11-13 12:49:42 +0530

    added logo

--- newscout/src/NavBar/index.js
+++ newscout/src/NavBar/index.js
@@ -6,7 +6,7 @@ export class NavBar extends React.Component {
 		return(
 			<Navbar>
         		<NavbarBrand href="/">
-        			<img src="../logo.jpg" />
+        			<img src="/static/images/logo/logo.jpg" />
         		</NavbarBrand>
           		<Nav className="ml-auto" navbar>
             		<NavItem>Components</NavItem>
