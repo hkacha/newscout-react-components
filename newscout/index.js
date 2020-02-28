56be340e4a77d674463ed6854547abf21140dcba
hardik <hardik@fafadiatech.com>
2020-02-28 17:02:43 +0530

    added footer component

--- newscout/index.js
+++ newscout/index.js
@@ -225,6 +225,104 @@ exports.Filter = Filter;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Footer = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactFontawesome = require("@fortawesome/react-fontawesome");
+
+var _freeBrandsSvgIcons = require("@fortawesome/free-brands-svg-icons");
+
+var _newscout = require("newscout");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var current_date = new Date();
+var current_year = current_date.getFullYear();
+
+var Footer =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Footer, _React$Component);
+
+  function Footer(props) {
+    var _this;
+
+    _classCallCheck(this, Footer);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Footer).call(this, props));
+    _this.state = {};
+    return _this;
+  }
+
+  _createClass(Footer, [{
+    key: "render",
+    value: function render() {
+      return _react.default.createElement("footer", {
+        className: "footer-area pt-3 mt-5"
+      }, _react.default.createElement("div", {
+        className: "container"
+      }, _react.default.createElement("div", {
+        className: "clearfix"
+      }, _react.default.createElement("div", {
+        className: "float-left"
+      }, _react.default.createElement("p", null, "\xA9 Copyright ", current_year, ", All Rights Reserved by ", _react.default.createElement("a", {
+        href: "/"
+      }, "Newscout"))), _react.default.createElement("div", {
+        className: "float-right"
+      }, _react.default.createElement("ul", {
+        className: "list-inline m-0"
+      }, _react.default.createElement("li", {
+        className: "list-inline-item"
+      }, _react.default.createElement("a", {
+        href: this.props.privacyurl
+      }, "Privacy Policy")), _react.default.createElement("li", {
+        className: "list-inline-item"
+      }, "|"), _react.default.createElement("li", {
+        className: "list-inline-item"
+      }, _react.default.createElement("a", {
+        href: this.props.facebookurl,
+        target: "_blank"
+      }, _react.default.createElement(_reactFontawesome.FontAwesomeIcon, {
+        icon: _freeBrandsSvgIcons.faFacebookSquare
+      }))), _react.default.createElement("li", {
+        className: "list-inline-item"
+      }, _react.default.createElement("a", {
+        href: this.props.twitterurl,
+        target: "_blank"
+      }, _react.default.createElement(_reactFontawesome.FontAwesomeIcon, {
+        icon: _freeBrandsSvgIcons.faTwitter
+      }))))))));
+    }
+  }]);
+
+  return Footer;
+}(_react.default.Component);
+
+exports.Footer = Footer;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 exports.HorizontalCardItem = void 0;
 
 var _react = _interopRequireDefault(require("react"));
---
+++ newscout/src/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+import { Search } from 'newscout';
+
+const current_date = new Date()
+const current_year = current_date.getFullYear()
+
+export class Footer extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+
+	render(){
+		return(
+			<footer className="footer-area pt-3 mt-5">
+				<div className="container">
+					<div className="clearfix">
+						<div className="float-left">
+							<p>&copy; Copyright {current_year}, All Rights Reserved by <a href="/">Newscout</a></p>
+						</div>
+						<div className="float-right">
+							<ul className="list-inline m-0">
+								<li className="list-inline-item">
+									<a href={this.props.privacyurl}>Privacy Policy</a>
+								</li>
+								<li className="list-inline-item">|</li>
+								<li className="list-inline-item">
+									<a href={this.props.facebookurl} target="_blank">
+										<FontAwesomeIcon icon={faFacebookSquare} />
+									</a>
+								</li>
+								<li className="list-inline-item">
+									<a href={this.props.twitterurl} target="_blank">
+										<FontAwesomeIcon icon={faTwitter} />
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+		)
+	}
+}
