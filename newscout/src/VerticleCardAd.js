eb9d26f98ba3507f9b3d6acedd1ae4a713b93c1c
hardik <hardik@fafadiatech.com>
2020-03-16 18:43:36 +0530

    working on ad functionality

---
+++ newscout/src/VerticleCardAd.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, CardBody, CardText } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+
+export class VerticleCardAd extends React.Component {
+
+	render(){
+		const { id, image, description, source_url } = this.props;
+		return(
+			<Card className="card-post">
+				<a href={`${source_url}`}><img src={image} alt={description} className="img-fluid" /></a>
+				<CardBody className="adlink">
+					<CardText className="card-desc"><a href={`${source_url}`} target="_blank">{description}</a></CardText>
+					<div className="clearfix">
+						<div className="float-left"></div>
+						<div className="float-right">
+							<ul className="list-inline m-0">
+								<li className="list-inline-item">
+									<FontAwesomeIcon icon={faExternalLinkSquareAlt} /> sponsored
+								</li>
+							</ul>
+						</div>
+					</div>
+				</CardBody>
+			</Card>
+		)
+	}
+}
