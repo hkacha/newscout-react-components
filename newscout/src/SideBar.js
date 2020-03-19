914e22f1d8a949e1edcdbf25bfe88187bd0f5bb9
hardik <hardik@fafadiatech.com>
2020-03-19 11:46:23 +0530

    revert changes

--- newscout/src/SideBar.js
+++ newscout/src/SideBar.js
@@ -8,12 +8,10 @@ export class SideBar extends React.Component {
 
 
 	render(){
-		var { menuitems, domain } = this.props;
-
-		var listitem = menuitems.map((item, index) => {
+		var listitem = this.props.menuitems.map((item, index) => {
 			return (
 				<ListGroupItem tag="a" href={"/"+item.itemurl+"/"} key={index}>
-					<img src={`${domain === "dashboard" ? '/' : '' }${item.item_icon}`} alt={item.itemtext} className="menu-icons"/>&nbsp;
+					<img src={`${item.item_icon}`} alt={item.itemtext} className="menu-icons"/>&nbsp;
 					{item.itemtext}
 				</ListGroupItem>
 			)
@@ -22,7 +20,7 @@ export class SideBar extends React.Component {
 		return(
 			<div className={`sidebar col-lg-2 d-none d-md-block d-xl-block ${this.props.class ? '' : 'sidebar-none'}`}>
 				<ListGroup>
-					{domain === "domain=newscout" || domain === undefined ?
+					{this.props.domain === "domain=newscout" || this.props.domain === undefined ?
 						<ListGroupItem tag="a" href="/news/trending/">
 							<img src="/static/icons/trending_news.png" alt="Trending" className="menu-icons"/>&nbsp;
 							Trending
