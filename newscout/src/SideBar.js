152093092a1d276c207a4787e8e621759e1b9101
hardik <hardik@fafadiatech.com>
2020-03-19 11:10:39 +0530

    fixed icon url issue

--- newscout/src/SideBar.js
+++ newscout/src/SideBar.js
@@ -8,10 +8,12 @@ export class SideBar extends React.Component {
 
 
 	render(){
-		var listitem = this.props.menuitems.map((item, index) => {
+		var { menuitems, domain } = this.props;
+
+		var listitem = menuitems.map((item, index) => {
 			return (
 				<ListGroupItem tag="a" href={"/"+item.itemurl+"/"} key={index}>
-					<img src={`${item.item_icon}`} alt={item.itemtext} className="menu-icons"/>&nbsp;
+					<img src={`${domain === "dashboard" ? '/' : '' }${item.item_icon}`} alt={item.itemtext} className="menu-icons"/>&nbsp;
 					{item.itemtext}
 				</ListGroupItem>
 			)
@@ -20,7 +22,7 @@ export class SideBar extends React.Component {
 		return(
 			<div className={`sidebar col-lg-2 d-none d-md-block d-xl-block ${this.props.class ? '' : 'sidebar-none'}`}>
 				<ListGroup>
-					{this.props.domain === "domain=newscout" || this.props.domain === undefined ?
+					{domain === "domain=newscout" || domain === undefined ?
 						<ListGroupItem tag="a" href="/news/trending/">
 							<img src="/static/icons/trending_news.png" alt="Trending" className="menu-icons"/>&nbsp;
 							Trending
