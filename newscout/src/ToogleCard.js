bd986601a29314d7f951430e89f1401d98d2708b
hardik <hardik@fafadiatech.com>
2020-03-20 15:00:30 +0530

    fixed ui for mobile

--- newscout/src/ToogleCard.js
+++ newscout/src/ToogleCard.js
@@ -17,40 +17,49 @@ export class ToogleCard extends React.Component {
 		var results = items.map((item, index) => {
 			if(index !== 0){
 				return (
-					<ul className="list-inline m-0" key={index}>
-						<li className="list-inline-item mb-4">
-							<h6 className="mb-1">{moment(item.published_on).format('D MMMM YYYY')}</h6>
-							<h4><a href={`/news/article/${item.slug}`}>{item.title}</a></h4>
-							<div className="clearfix">
-								<div className="float-left">
-									<h6 className="m-0">
-										<a href={item.source_url}>{item.source}</a>
-									</h6>
-								</div>
-								<div className="float-right">
-									<ul className="list-inline m-0 sharelink">
-										<li className="list-inline-item">
-											<div>
-												<FacebookShareButton url={`${base_url}/news/article/${item.slug}`} quote={item.title}>
-													<FacebookIcon size={15} round />
-												</FacebookShareButton>&nbsp;
-												<TwitterShareButton url={`${base_url}/news/article/${item.slug}`} quote={item.title}>
-													<TwitterIcon size={15} round />
-												</TwitterShareButton>&nbsp;
-												<WhatsappShareButton url={`${base_url}/news/article/${item.slug}`} quote={item.title}>
-													<WhatsappIcon size={15} round />
-												</WhatsappShareButton>
-											</div>
-											<FontAwesomeIcon icon={faShareAlt} />
-										</li>
-										<li className="list-inline-item">
-											<FontAwesomeIcon icon={faBookmark} onClick={this.getArticleId} className={bookmark_ids.indexOf(item.id) > -1 ? 'bookmarked' : ''} />
-										</li>
-									</ul>
-								</div>
-							</div>
-						</li>
-					</ul>
+					<div className="row">
+						<div className="col-lg-9 col-12">
+							<ul className="list-inline m-0 inside-post" key={index}>
+								<li className="list-inline-item mb-4">
+									<h6 className="mb-1">{moment(item.published_on).format('D MMMM YYYY')}</h6>
+									<h4><a href={`/news/article/${item.slug}`}>{item.title}</a></h4>
+									<div className="clearfix">
+										<div className="float-left">
+											<h6 className="m-0">
+												<a href={item.source_url}>{item.source}</a>
+											</h6>
+										</div>
+										<div className="float-right">
+											<ul className="list-inline m-0 sharelink">
+												<li className="list-inline-item">
+													<div>
+														<FacebookShareButton url={`${base_url}/news/article/${item.slug}`} quote={item.title}>
+															<FacebookIcon size={15} round />
+														</FacebookShareButton>&nbsp;
+														<TwitterShareButton url={`${base_url}/news/article/${item.slug}`} quote={item.title}>
+															<TwitterIcon size={15} round />
+														</TwitterShareButton>&nbsp;
+														<WhatsappShareButton url={`${base_url}/news/article/${item.slug}`} quote={item.title}>
+															<WhatsappIcon size={15} round />
+														</WhatsappShareButton>
+													</div>
+													<FontAwesomeIcon icon={faShareAlt} />
+												</li>
+												<li className="list-inline-item">
+													<FontAwesomeIcon icon={faBookmark} onClick={this.getArticleId} className={bookmark_ids.indexOf(item.id) > -1 ? 'bookmarked' : ''} />
+												</li>
+											</ul>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div className="col-lg-3 text-right col-12 d-none d-sm-block">
+							<a href={`/news/article/${item.slug}`}>
+								<img src={"http://images.newscout.in/unsafe/70x70/left/top/"+decodeURIComponent(item.cover_image)} className="img-fluid" />
+							</a>
+						</div>
+					</div>
 				)
 			}
 		})
@@ -61,6 +70,11 @@ export class ToogleCard extends React.Component {
 					<div className="card-header" id={`heading${index}`}>
 						<div className="article-card">
 							<div className="row">
+								<div className="col-lg-3 text-right col-12 d-block d-sm-none mb-1">
+									<a href={`/news/article/${items[0].slug}`}>
+										<img src={"http://images.newscout.in/unsafe/368x276/left/top/"+decodeURIComponent(items[0].cover_image)} className="img-fluid" />
+									</a>
+								</div>
 								<div className="col-lg-9 col-12">
 									<ul className="list-inline featurepost-category mb-1">
 										<li className="list-inline-item">{moment(items[0].published_on).format('D MMMM YYYY')}</li>
@@ -95,7 +109,7 @@ export class ToogleCard extends React.Component {
 										</div>
 									</div>
 								</div>
-								<div className="col-lg-3 text-right col-12">
+								<div className="col-lg-3 text-right col-12 d-none d-sm-block">
 									<a href={`/news/article/${items[0].slug}`}>
 										<img src={"http://images.newscout.in/unsafe/100x100/left/top/"+decodeURIComponent(items[0].cover_image)} className="img-fluid" />
 									</a>
